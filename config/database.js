7862f6cc1390fa0c2e0c11f07d340e6b308f81a6
jeffvilla <jvilla125@gmail.com>
2023-03-19 13:08:06 -0700

    update database

--- config/database.js
+++ config/database.js
@@ -3,12 +3,12 @@ const mongoose = require('mongoose');
 // // replace your database connection string here
 // mongoose.connect(process.env.DATABASE_URL);
 
-const db = mongoose.connection;
+// const db = mongoose.connection;
 
 // database connection event
-db.on('connected', function () {
-  console.log(`Mongoose connected to: ${db.host}:${db.port}`);
-});
+// db.on('connected', function () {
+//   console.log(`Mongoose connected to: ${db.host}:${db.port}`);
+// });
 
 // export the function that creates a database connection
 module.exports = {
